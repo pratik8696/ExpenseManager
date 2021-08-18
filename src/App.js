3b83b44e04e3cf7cac377191e4b6dc5f66d6e61f
pratik8696 <mepratikanand@gmail.com>
2021-08-18 16:32:54 +0530

    stateful list done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+
+const expenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
+  
+  const [list,setlist]=useState([
+    ...expenses
+  ])
+
 
   // return React.createElement(
   //   'div',
@@ -36,13 +45,18 @@ const App = () => {
   function handledataapp(data)
   {
     console.log(data);
-    console.log(expenses);
+    setlist((prevValue)=>{
+      return ({
+        ...prevValue,
+        ...data
+      })
+    })
   }
 
   return (
     <div>
       <NewExpense datainapp={handledataapp} />
-      <Expenses items={expenses} />
+      <Expenses items={list} />
     </div>
   );
 }
